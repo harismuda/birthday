991d774a6f8250cd7668447ffab0799b488a834b
harismuda <harismuda20102003@gmail.com>
2024-12-18 23:40:54 +0700

    Update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const birthday = new Date(new Date().getFullYear(), 11, 25);
 // const birthday = new Date(new Date().getTime() + 60 * 1000);
 
-// Kalau tanggal sekarang lewat ulang tahun, set untuk tahun depan
 if (new Date() > birthday) {
   birthday.setFullYear(birthday.getFullYear() + 1);
 }
@@ -11,25 +10,21 @@ function updateCountdown() {
   const diff = birthday - now;
   // const diff = 0;
 
-  // Hitung waktu tersisa
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((diff / (1000 * 60)) % 60);
   const seconds = Math.floor((diff / 1000) % 60);
 
-  // Update elemen HTML
   document.getElementById('days').textContent = days.toString().padStart(2, '0');
   document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
   document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
   document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
 
-  // Cek apakah countdown selesai
   if (diff <= 0) {
-    // clearInterval(countdownInterval);
-    window.location.href = "ucapan.html"; // Redirect ke halaman tujuan
+    clearInterval(countdownInterval);
+    window.location.href = "ucapan.html";
   }
 }
 
-// Perbarui hitungan mundur setiap 1 detik
 const countdownInterval = setInterval(updateCountdown, 1000);
 updateCountdown();
